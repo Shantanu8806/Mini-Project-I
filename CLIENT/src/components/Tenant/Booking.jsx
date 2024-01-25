2896c95986f173fe712d3d1d55c9be7527cc5363
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2024-01-25 23:39:05 +0530

    Update Booking.jsx

--- CLIENT/src/components/Tenant/Booking.jsx
+++ CLIENT/src/components/Tenant/Booking.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 import mapboxgl from 'mapbox-gl';
 import TimePicker from 'react-time-picker';
 import 'react-time-picker/dist/TimePicker.css';
+import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
-const Booking = () => {
-  const [numberOfHours, setNumberOfHours] = useState(0); 
+const Booking = ({ logged, user, setUser, setLogged }) => {
+  const [numberOfHours, setNumberOfHours] = useState(0);
   const navigate = useNavigate();
   const { spaceId } = useParams();
   const [parkingSpace, setParkingSpace] = useState(null);
@@ -16,45 +18,46 @@ const Booking = () => {
   const [bookingCost, setBookingCost] = useState(null);
   const [mapboxToken, setMapboxToken] = useState('');
   const [userLocation, setUserLocation] = useState(null);
+  const [tenantId, setTenantId] = useState(null);
 
   useEffect(() => {
+    if (!logged) {
+      navigate('/login/tenant');
+    }
+    if (user !== 'Tenant') {
+      navigate('/login/tenant');
+    }
     const fetchMapboxToken = async () => {
       try {
         const response = await axios.get('http://localhost:4000/api/v1/mapbox/mapbox-token');
         setMapboxToken(response.data.mapboxToken);
-  
-        // Get the user's current location using the Geolocation API
+
         navigator.geolocation.getCurrentPosition(
           async (position) => {
             const { latitude, longitude } = position.coords;
             setUserLocation([longitude, latitude]);
-  
-            // Fetch and set parking space details
+
             try {
               const parkingSpaceResponse = await axios.get(`http://localhost:4000/api/v1/parking-space/spaceDetails/${spaceId}`);
               setParkingSpace(parkingSpaceResponse.data.parkingSpaceDetails);
-  
-              // Initialize the map with Mapbox access token
+
               mapboxgl.accessToken = response.data.mapboxToken;
-  
+
               const map = new mapboxgl.Map({
                 container: 'map',
                 style: 'mapbox://styles/mapbox/streets-v11',
-                center: [longitude, latitude], // Center on the user's location
+                center: [longitude, latitude],
                 zoom: 12,
               });
-  
-              // Add a blue marker for the user's current location
+
               const userMarker = new mapboxgl.Marker({ color: 'blue' })
                 .setLngLat([longitude, latitude])
                 .addTo(map);
-  
-              // Add a red marker for the parking space location
+
               const parkingSpaceMarker = new mapboxgl.Marker({ color: 'red' })
                 .setLngLat([parkingSpaceResponse.data.parkingSpaceDetails.longitude, parkingSpaceResponse.data.parkingSpaceDetails.latitude])
                 .addTo(map);
-  
-              // Fetch the route between user location and parking space
+
               const directionsResponse = await axios.get(
                 `https://api.mapbox.com/directions/v5/mapbox/driving-traffic/${longitude},${latitude};${parkingSpaceResponse.data.parkingSpaceDetails.longitude},${parkingSpaceResponse.data.parkingSpaceDetails.latitude}`,
                 {
@@ -65,10 +68,9 @@ const Booking = () => {
                   },
                 }
               );
-  
+
               const route = directionsResponse.data.routes[0].geometry;
-              
-              // Add the route layer to the map
+
               map.on('load', () => {
                 map.addSource('route', {
                   type: 'geojson',
@@ -78,7 +80,7 @@ const Booking = () => {
                     geometry: route,
                   },
                 });
-  
+
                 map.addLayer({
                   id: 'route',
                   type: 'line',
@@ -93,8 +95,7 @@ const Booking = () => {
                   },
                 });
               });
-  
-              // Update the route and markers as the user moves
+
               const updateRoute = async () => {
                 const newPosition = [userMarker.getLngLat().lng, userMarker.getLngLat().lat];
                 const directionsResponse = await axios.get(
@@ -104,14 +105,13 @@ const Booking = () => {
                       access_token: response.data.mapboxToken,
                       overview: 'full',
                       steps: true,
-                      geometries: 'geojson', // Specify the geometries format
+                      geometries: 'geojson',
                     },
                   }
                 );
-              
+
                 const newRoute = directionsResponse.data.routes[0].geometry;
-              
-                // Convert the route to a valid GeoJSON LineString
+
                 const geojson = {
                   type: 'Feature',
                   properties: {},
@@ -120,11 +120,10 @@ const Booking = () => {
                     coordinates: newRoute.coordinates,
                   },
                 };
-              
+
                 map.getSource('route').setData(geojson);
               };
-            
-              // Update the route when the user moves
+
               map.on('move', updateRoute);
             } catch (error) {
               console.error('Error fetching parking space details:', error);
@@ -138,15 +137,20 @@ const Booking = () => {
         console.error('Error fetching Mapbox token:', error);
       }
     };
-  
+
     fetchMapboxToken();
-  }, [spaceId, mapboxToken]);
-  
-  
-  
+  }, [spaceId, mapboxToken, logged, user, navigate]);
 
   useEffect(() => {
-    // Update cost based on the selected booking type and number of hours
+    const token = sessionStorage.getItem('token');
+    if (!token) {
+      console.error('User not authenticated.');
+      return;
+    }
+    const decodedToken = jwtDecode(token);
+    const userId = decodedToken.userId;
+    setTenantId(userId);
+
     switch (selectedBookingType) {
       case 'hourly':
         setBookingCost(parkingSpace?.hourlyCost * numberOfHours);
@@ -167,21 +171,122 @@ const Booking = () => {
 
   const handleNumberOfHoursChange = (event) => {
     const hours = parseInt(event.target.value, 10);
-    setNumberOfHours(isNaN(hours) ? 0 : hours); // Set to 0 if input is not a valid number
+    setNumberOfHours(isNaN(hours) ? 0 : hours);
   };
 
   const handleBookingTypeChange = (event) => {
-    setSelectedBookingType(event.target.value);
+    const selectedType = event.target.value;
+    setSelectedBookingType(selectedType);
+
+    switch (selectedType) {
+      case 'hourly':
+        const hours = parseInt(event.target.value, 10);
+        setNumberOfHours(isNaN(hours) ? 0 : hours);
+        break;
+      case 'daily':
+        setNumberOfHours(24);
+        break;
+      case 'weekly':
+        setNumberOfHours(24 * 7);
+        break;
+      case 'monthly':
+        setNumberOfHours(24 * 30);
+        break;
+      default:
+        setNumberOfHours(0);
+    }
   };
 
   const handleTimeChange = (time) => {
     setSelectedTime(time);
   };
 
-  const handleProceedToPayment = () => {
-    // Implement redirection to payment page or any other logic based on selectedBookingType, selectedTime, and bookingCost
-    // You may use react-router-dom history for navigation or any other navigation method
-    // Example: history.push('/payment');
+  const initiateBookingAfterPayment = async () => {
+    try {
+      navigate('/tenantdashboard');
+      toast.success("Payment Successful");
+      toast.success("Parking Space Booked Successfully");
+    } catch (error) {
+      console.error('Error during payment verification and booking initiation:', error);
+      toast.error("Error during payment verification and booking initiation");
+    }
+  };
+
+  const checkouthandler = async (amount, selectedTime) => {
+    try {
+      if (window.razorpay) {
+        window.razorpay.close();
+      }
+  
+      const { data: { key } } = await axios.get("http://localhost:4000/api/v1/booking/getRazorpayKey");
+      const bookingDataString = JSON.stringify({
+        tenantId,
+        parkingSpaceId: spaceId,
+        typeofBooking: selectedBookingType,
+        totalTimeInHours: numberOfHours,
+      });
+      const encodedBookingData = encodeURIComponent(bookingDataString);
+  
+      const { data: { order } } = await axios.post(`http://localhost:4000/api/v1/booking/createOrder`, { amount, selectedTime });
+  
+      const options = {
+        key,
+        amount: order.amount,
+        currency: "INR",
+        name: "PARKIT",
+        description: "Razorpay tutorial",
+        image: "https://res.cloudinary.com/dnmlmaz3l/image/upload/v1704824872/Parking/ef9oahebfnru5ca35teh.jpg",
+        order_id: order.id,
+        callback_url: `http://localhost:4000/api/v1/booking/paymentVerification?bookingData=${encodedBookingData}`,
+        prefill: {
+          name: "Shantanu kantak",
+          email: "kantakshantanu20@gmail.com",
+          contact: "9372271678"
+        },
+        notes: {
+          address: "Razorpay official",
+          time: selectedTime
+        },
+        theme: {
+          "color": "#3399cc"
+        }
+      };
+  
+      const razor = new window.Razorpay(options);
+  
+      // Attach a callback function to handle payment success
+      razor.on('payment.success', async function (successResponse) {
+        try {
+          // Initiate booking after payment
+          await initiateBookingAfterPayment();
+        } catch (error) {
+          console.error('Error during booking initiation after payment:', error);
+          toast.error("Error during booking initiation after payment");
+        }
+      });
+  
+      razor.open();
+      window.razorpay = razor;
+    } catch (error) {
+      console.error('Error during Razorpay checkout:', error);
+      toast.error("Error during Razorpay checkout");
+    }
+  };
+  
+
+  const handleProceedToPayment = async () => {
+    if (!selectedTime || !bookingCost || !parkingSpace) {
+      return;
+    }
+
+    const totalAmountInPaise = bookingCost;
+
+    try {
+      await checkouthandler(totalAmountInPaise, selectedTime);
+    } catch (error) {
+      console.error('Error during Razorpay checkout:', error);
+      toast.error("Error during Razorpay checkout");
+    }
   };
 
   const handleGoBack = () => {
@@ -192,7 +297,7 @@ const Booking = () => {
     <div className="w-full flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-4">Booking Details</h1>
       <div id="map" className="w-full h-80 mb-4" />
-  
+
       {parkingSpace && (
         <div className="bg-white p-8 rounded-md shadow-md w-[80%] max-w-xl">
           <img
@@ -200,7 +305,7 @@ const Booking = () => {
             alt="Parking Space"
             className="mb-4 mx-auto object-cover rounded-md w-full h-[40%]"
           />
-  
+
           <div className="text-center mb-4">
             <p className="text-lg font-bold">{parkingSpace.address}</p>
             <p>Hourly Cost: {parkingSpace.hourlyCost}</p>
@@ -208,7 +313,7 @@ const Booking = () => {
             <p>Weekly Cost: {parkingSpace.weeklyCost}</p>
             <p>Monthly Cost: {parkingSpace.monthlyCost}</p>
           </div>
-  
+
           <div className="flex flex-col items-center mb-4">
             <label htmlFor="bookingType" className="mb-2 text-lg font-bold">
               Select Booking Type:
@@ -226,7 +331,7 @@ const Booking = () => {
               <option value="monthly">Monthly</option>
             </select>
           </div>
-  
+
           {selectedBookingType === 'hourly' && (
             <div className="mb-4">
               <label htmlFor="selectedTime" className="text-lg font-bold mb-2">
@@ -242,7 +347,7 @@ const Booking = () => {
               />
             </div>
           )}
-  
+
           {selectedBookingType === 'hourly' && (
             <div className="mb-4">
               <label htmlFor="numberOfHours" className="text-lg font-bold mb-2">
@@ -258,13 +363,13 @@ const Booking = () => {
               />
             </div>
           )}
-  
+
           {selectedBookingType && (
             <div className="text-lg font-bold mb-2">
               Booking Cost: {bookingCost}
             </div>
           )}
-  
+
           {selectedBookingType && (
             <div className="flex justify-center space-x-4">
               <button
@@ -286,7 +391,6 @@ const Booking = () => {
       )}
     </div>
   );
-  
 };
 
 export default Booking;
