2151e32bedcfa711d7805bdadece71d82893f2d5
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2024-01-26 10:34:51 +0530

    Update bookingroutes.js

--- Routes/bookingroutes.js
+++ Routes/bookingroutes.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { bookParkingSpace, getAllPreviousBookings, getPendingBookings } = require('../controllers/Booking');
+const {createOrder,getRazorpayKeyController,paymentVerificationController} = require('../controllers/Payment');
+const { initiateBookingAfterPayment, getAllPreviousBookingsByOwner, getPendingBookings } = require('../controllers/Booking');
 const { authenticateTenant, authenticateOwner } = require('../middlewares/auth');
+
 
 // Middleware for Cloudinary file uploads (if needed)
 // const cloudinaryUpload = require('../middlewares/cloudinaryUpload');
 
 // Routes for booking operations
-router.post('/book-parking-space', authenticateTenant, /* cloudinaryUpload, */ bookParkingSpace);
+router.post('/book-parking-space', authenticateTenant, /* cloudinaryUpload, */ initiateBookingAfterPayment);
 
 
 
 
 
 // Route for owners to get all previous bookings
-router.get('/previous-bookings',getAllPreviousBookings);
+router.get('/previous-bookings',authenticateOwner,getAllPreviousBookingsByOwner);
+router.get('/getRazorpayKey',getRazorpayKeyController);
+router.post('/createOrder',createOrder);
+router.post('/paymentVerification',initiateBookingAfterPayment);
 
 // router.get('/pending-bookings',getPendingBookings);
 
 module.exports = router;
+
