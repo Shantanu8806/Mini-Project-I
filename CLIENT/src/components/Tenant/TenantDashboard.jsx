5a3c30a8605ec6f54403dac4844dd6812a0c0f27
Shantanu8806 <119732714+Shantanu8806@users.noreply.github.com>
2024-02-01 18:42:29 +0530

    Update TenantDashboard.jsx

--- CLIENT/src/components/Tenant/TenantDashboard.jsx
+++ CLIENT/src/components/Tenant/TenantDashboard.jsx
@@ -3,19 +3,29 @@ import axios from 'axios';
 import mapboxgl from 'mapbox-gl';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-const TenantDashboard = ({logged,user,setUser,setLogged}) => {
+import { jwtDecode } from 'jwt-decode';
+const TenantDashboard = ({ logged, user, setUser, setLogged }) => {
   const navigate = useNavigate();
   const [mapboxToken, setMapboxToken] = useState('');
   const [selectedParkingSpace, setSelectedParkingSpace] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [userCoordinates, setUserCoordinates] = useState(null);
   const [parkingSpaces, setParkingSpaces] = useState([]);
-
+  const [activeBooking, setActiveBooking] = useState([]);
+  const [tenantId, setTenantId] = useState('');
   useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    if (!token) {
+      console.error('User not authenticated.');
+      return;
+    }
+    const decodedToken = jwtDecode(token);
+    const userId = decodedToken.userId;
+    setTenantId(userId);
     if (!logged) {
       navigate('/login/tenant');
     }
-    if(user!=='Tenant'){
+    if (user !== 'Tenant') {
       navigate('/login/tenant');
     }
     const fetchMapboxToken = async () => {
@@ -48,7 +58,7 @@ const TenantDashboard = ({logged,user,setUser,setLogged}) => {
     const fetchParkingSpaces = async () => {
       try {
         const response = await axios.get('http://localhost:4000/api/v1/parking-space/parking-spaceNotOccupied');
-        
+
         // Check if the response has the expected structure
         if (response.data && Array.isArray(response.data.parkingSpaces)) {
           setParkingSpaces(response.data.parkingSpaces);
@@ -57,6 +67,14 @@ const TenantDashboard = ({logged,user,setUser,setLogged}) => {
         }
       } catch (error) {
         console.error('Error fetching parking spaces:', error);
+      }
+    };
+    const fetchActiveBookings = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/api/v1/booking/getActiveBookingsByTenantId?tenantId=659d3d32805742c80fe5a60e`);
+        setActiveBooking(response.data);
+      } catch (error) {
+        console.error('Error fetching active bookings:', error);
       }
     };
 
@@ -69,9 +87,9 @@ const TenantDashboard = ({logged,user,setUser,setLogged}) => {
         console.error('Error fetching data:', error);
       }
     };
-
+    fetchActiveBookings();
     fetchLocationAndParkingSpaces();
-  }, [logged,user,navigate]);
+  }, [logged, user, tenantId, navigate]);
 
   useEffect(() => {
     if (!mapboxToken || !userCoordinates) {
@@ -132,19 +150,44 @@ const TenantDashboard = ({logged,user,setUser,setLogged}) => {
           <p>Weekly Cost: {selectedParkingSpace.weeklyCost}</p>
           <p>Monthly Cost: {selectedParkingSpace.monthlyCost}</p>
           <div className="flex justify-end mt-2">
-            <button className="bg-blue-500 text-white p-2 mr-2" onClick={() => setShowModal(false)}>
+            <button className="bg-blue-500 text-white p-2 mr-2 rounded-lg" onClick={() => setShowModal(false)}>
               Close
             </button>
             <Link to={`/Booking/${selectedParkingSpace._id}`}>
-            <button className="bg-green-500 text-white p-2">
-              Book Now
-            </button>
+              <button className="bg-green-500 text-white p-2 rounded-lg">
+                Book Now
+              </button>
             </Link>
           </div>
         </div>
       )}
 
-      <button className="bg-green-500 text-white p-2 mt-4">Search Location</button>
+      <button className="bg-green-500 text-white p-2 mt-4 rounded-lg">Search Location</button>
+      {/* Active Bookings Section */}
+      <div className="mt-4">
+        <h2 className="text-xl font-bold mb-2">Active Bookings</h2>
+        {activeBooking.map((booking) => (
+          <div key={booking._id} className="bg-white rounded-md shadow-md p-4 mb-4">
+            <img
+              src={booking.parkingSpace.parkingSpaceImage}
+              alt="Parking Space"
+              className="w-18 h-18 rounded-md mt-2"
+            />
+
+            <h3 className="text-xl font-bold mb-2">Booking Information</h3>
+            <p>{`Parking Number: ${booking.parkingSpace.parkingNumber}`}</p>
+            <p>{`Area Description: ${booking.parkingSpace.parkingAreaDescription}`}</p>
+            <p>{`Amount Paid: ₹${booking.amountPaid}`}</p>
+            <p>{`Addres: ${booking.parkingSpace.address}`}</p>
+            {/* Add more details as needed */}
+          </div>
+        ))}
+      </div>
+
+      {/* Button to navigate to Previous Bookings component */}
+      <Link to="/previous-bookings">
+        <button className="bg-blue-500 text-white p-2 mt-4 rounded-lg">Previous Bookings</button>
+      </Link>
     </div>
   );
 };
